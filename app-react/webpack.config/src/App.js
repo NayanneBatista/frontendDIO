54366d58172f89ae13f88664ffb0037601ac3527
Nayanne Batista <nayanne.lb@gmail.com>
2021-02-01 11:58:04 -0300

    Configuração do webpack

---
+++ app-react/webpack.config/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+    return (
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Introdução ao React JS. Xero!</p>
+        </div>
+    );
+};
+export default App;
